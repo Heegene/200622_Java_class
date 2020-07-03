3735dcf7347a9355fc5b29297ffc33a77df8a8d1
Heegene <dev.heejin@gmail.com>
2020-07-03 17:58:39 +0900

    200703 sql db management, table space

--- SQL sources/200703_sql_db management.sql
+++ SQL sources/200703_sql_db management.sql
@@ -14,3 +14,11 @@ GRANT READ, WRITE ON DIRECTORY mdBackUP2 TO scott3;
 CREATE TABLESPACE user1 datafile 'C:\Java Programming\tablespace\user1.ora' size 100m;
 CREATE TABLESPACE user2 datafile 'C:\Java Programming\tablespace\user2.ora' size 100m;
 CREATE TABLESPACE user3 datafile 'C:\Java Programming\tablespace\user3.ora' size 100m;
+
+
+
+-- 유저생성과 동시에 테이블스페이스 할당 가능 
+CREATE USER scott2 IDENTIFIED BY tiger
+DEFAULT TABLESPACE user1;
+
+GRANT DBA TO scott2;
