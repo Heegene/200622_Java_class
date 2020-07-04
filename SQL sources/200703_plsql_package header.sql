1ccd24778168ddd5bc8e2b261efca690bb42beaa
Heegene <64513810+Heegene@users.noreply.github.com>
2020-07-04 23:16:19 +0900

    200704 sql update

--- SQL sources/200703_plsql_package header.sql
+++ SQL sources/200703_plsql_package header.sql
@@ -1,8 +1,11 @@
 create or replace PACKAGE emp_info AS 
     PROCEDURE all_sal_info;
-    PROCEDURE all_emp_info; -- ��� ����� ����
-    -- Ư�� �μ��� ��� ����
+    PROCEDURE all_emp_info; -- 모든 사원의 정보
+    -- 특정 부서의 사원 정보
     PROCEDURE dept_emp_info(v_deptno IN NUMBER);
-    -- Ư�� �μ��� �޿� ����
+    -- 특정 부서의 급여 정보
     PROCEDURE dept_sal_info(v_deptno IN NUMBER);
 END emp_info;
+
+-- 헤더이므로 들어가야하는 프로시저의 정보만 담음
+-- 내용은 바디 안에 프로시저별로 정의할 것 
