8157d62796cd5b3f462740d8fadbd393b4e97d3f
Heegene <dev.heejin@gmail.com>
2020-07-08 10:01:54 +0900

    200708 JDBC connection test

---
+++ ch18/src/ch18/OraDr.java
@@ -0,0 +1,39 @@
+package ch18;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class OraDr {
+	private static String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	// 여기서 127.0.0.1을 localhost로 변경해줘도 됨 
+	private static String user = "scott";
+	private static String passwd = "tiger";
+	public static void main(String[] args) {
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		
+		try {
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url, user, passwd);
+			System.out.println("Start");
+			
+			
+			if( conn != null) {
+				System.out.println("연결 성공");
+			}
+			else {
+				System.out.println("연결 실패");
+			}
+			
+			conn.close();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
