d50619ed64c7180eb105da5ccb87193ddde4d2e2
Heegene <dev.heejin@gmail.com>
2020-07-08 11:10:55 +0900

    200708 JDBC ex2 update

--- ch18/src/ch18/OraDr.java
+++ ch18/src/ch18/OraDr.java
@@ -4,10 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class OraDr {
-	private static String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-	// 여기서 127.0.0.1을 localhost로 변경해줘도 됨 
-	private static String user = "scott";
-	private static String passwd = "tiger";
+	private static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	// 여기서 127.0.0.1을 localhost로 변경해줘도 됨 1521 -> 포트번호 xe => service id(sid)
+	private static final String user = "scott";
+	private static final String passwd = "tiger";
 	public static void main(String[] args) {
 		
 		String driver = "oracle.jdbc.driver.OracleDriver";
@@ -25,12 +25,15 @@ public class OraDr {
 				System.out.println("연결 실패");
 			}
 			
+			
 			conn.close();
 			
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
+		
+		
 		
 		
 		
---
+++ ch18/src/ch18/OraInsert.java
@@ -0,0 +1,60 @@
+package ch18;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class OraInsert {
+	private static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	// 여기서 127.0.0.1을 localhost로 변경해줘도 됨 1521 -> 포트번호 xe => service id(sid)
+	private static final String user = "scott";
+	private static final String passwd = "tiger";
+	public static void main(String[] args) throws SQLException {
+		Scanner sc = new Scanner(System.in);
+				System.out.println("부서번호 입력");
+				String deptno = sc.nextLine();
+				
+				System.out.println("부서명 입력 ");
+				String dname = sc.nextLine();
+				
+				System.out.println("위치 입력");
+				String loc = sc.nextLine();
+				
+				
+				Connection conn = null;
+				Statement stmt = null;
+				String driver = "oracle.jdbc.driver.OracleDriver";
+				String sql = String.format("INSERT INTO DEPT VALUES(%s, '%s', '%s')", deptno, dname, loc);
+				
+				try {
+					Class.forName(driver);
+					conn = DriverManager.getConnection(url, user, passwd);
+					
+					stmt = conn.createStatement();
+					
+					int result = stmt.executeUpdate(sql);
+					if (result > 0) {
+						System.out.println("입력 성공");
+					} else {
+						System.out.println("입력 실패");
+						
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				} finally {
+					if (stmt != null) {
+						stmt.close();
+					}
+					if (conn != null) {
+						conn.close();
+					}
+				}
+				
+		
+		
+	}
+
+}
